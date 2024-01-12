fa84f014837db864a3cf275df3f71bd2cff193d8
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-12 12:19:29 -0300

    add 01-useState

--- 09-hooks/src/HooksApp.jsx
+++ 09-hooks/src/HooksApp.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import CounterApp from '01-useState/CounterApp'
 
 function HooksApp() {
   return (
-    <div>HooksApp</div>
+    <div className="vh-100 d-flex align-items-center justify-content-center bg-body-tertiary">
+      <div className="p-4 bg-white shadow-sm rounded-4">
+        <CounterApp />
+      </div>
+    </div>
   )
 }
 

cb6f797bee7cf80ad1c34b799bff5f2a2ffcfb1c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-17 10:46:53 -0300

    add 06-memos/MemoHook

--- 09-hooks/src/HooksApp.jsx
+++ 09-hooks/src/HooksApp.jsx
@@ -9,6 +9,7 @@ import MultipleHooks from '03-examples/MultipleHooks'
 import FocusScreen from '04-useRef/FocusScreen'
 import Layout from '05-useLayoutEffect/Layout'
 import Memorize from '06-memos/Memorize'
+import MemoHook from '06-memos/MemoHook'
 
 const mainComponents = {
   CounterApp,
@@ -18,7 +19,8 @@ const mainComponents = {
   MultipleHooks,
   FocusScreen,
   Layout,
-  Memorize
+  Memorize,
+  MemoHook,
 }
 
 function HooksApp() {

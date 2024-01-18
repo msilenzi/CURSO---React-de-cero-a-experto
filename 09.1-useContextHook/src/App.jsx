aa5aeb3f810beafaebffb935b7f86969be988396
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-18 10:18:37 -0300

    install react-bootstrap and react-router

--- 09.1-useContextHook/src/App.jsx
+++ 09.1-useContextHook/src/App.jsx
@@ -1,8 +1,19 @@
-function App() {
+import { Navigate, Route, Routes } from 'react-router-dom'
+import About from '@Pages/About'
+import Home from '@Pages/Home'
+import Login from '@Pages/Login'
 
+function App() {
   return (
     <>
       <h1>App</h1>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<Navigate to={'/about'} />} />
+      </Routes>
     </>
   )
 }

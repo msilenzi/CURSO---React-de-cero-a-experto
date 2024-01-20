cf86d0a1bf4cd5e146895f9a5cfd8bca68414dd6
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-19 22:02:00 -0300

    add jest path mapping

--- 09-hooks/jest.config.cjs
+++ 09-hooks/jest.config.cjs
@@ -1,5 +1,11 @@
+const { pathsToModuleNameMapper } = require('ts-jest')
+const { compilerOptions } = require('./jsconfig')
+
 /** @type {import('jest').Config} */
 module.exports = {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['jest-extended/all', './jest.setup.js'],
+  roots: ['<rootDir>'],
+  modulePaths: [compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
 }

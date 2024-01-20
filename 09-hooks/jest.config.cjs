c5f12d8680b56a250c8d5d747e418d191049f527
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-20 02:39:14 -0300

    add compatibility with css modules and fetch

--- 09-hooks/jest.config.cjs
+++ 09-hooks/jest.config.cjs
@@ -7,5 +7,10 @@ module.exports = {
   setupFilesAfterEnv: ['jest-extended/all', './jest.setup.js'],
   roots: ['<rootDir>'],
   modulePaths: [compilerOptions.baseUrl],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  moduleNameMapper: {
+    ...pathsToModuleNameMapper(compilerOptions.paths),
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
+    '\\.(css|less)$': 'identity-obj-proxy',
+  }
+  
 }

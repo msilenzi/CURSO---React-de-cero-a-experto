b79300ea2a9ba1f233ece32cbf97b253dafbfdf8
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-22 19:34:51 -0300

    Remove unused imports

--- 09.1-useContextHook/src/__tests__/App.test.jsx
+++ 09.1-useContextHook/src/__tests__/App.test.jsx
@@ -1,4 +1,4 @@
-import { prettyDOM, render, screen } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from 'react-router-dom'
 import App from 'App'
 

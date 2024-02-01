4dc971faa82d4328135b1ccc608fdcd17b1c0e65
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-01-31 21:55:38 -0300

    improve <Card3d /> performance

--- 14-heroes-app/src/components/ui/Card3d/index.jsx
+++ 14-heroes-app/src/components/ui/Card3d/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef } from 'react'
 import PropTypes from 'prop-types'
 import './Card3d.css'
 
@@ -6,40 +6,28 @@ function map(val, minA, maxA, minB, maxB) {
   return minB + ((val - minA) * (maxB - minB)) / (maxA - minA)
 }
 
-const initialStyles = {
-  transform: 'rotateX(0deg) rotateY(0deg)',
-  filter: 'brightness(1)',
-}
-
 function Card3d({ children }) {
-  const [styles, setStyles] = useState(initialStyles)
-  const animationFrameRef = useRef(null)
+  const cardRef = useRef(null)
 
   function handleMouseMove(e) {
-    cancelAnimationFrame(animationFrameRef.current)
-
     const limit = 15
     const boundingBox = e.currentTarget.getBoundingClientRect()
 
-    animationFrameRef.current = requestAnimationFrame(() => {
-      let offsetX = e.nativeEvent.clientX - boundingBox.left
-      let offsetY = e.nativeEvent.clientY - boundingBox.top
+    let offsetX = e.nativeEvent.clientX - boundingBox.left
+    let offsetY = e.nativeEvent.clientY - boundingBox.top
 
-      let rotateY = map(offsetX, 0, boundingBox.width, -limit, limit)
-      let rotateX = map(offsetY, 0, boundingBox.height, limit, -limit)
-      let brightness = map(offsetY, 0, boundingBox.height, 1.25, 0.75)
+    let rotateY = map(offsetX, 0, boundingBox.width, -limit, limit)
+    let rotateX = map(offsetY, 0, boundingBox.height, limit, -limit)
+    let brightness = map(offsetY, 0, boundingBox.height, 1.25, 0.75)
 
-      setStyles({
-        transform: `rotateX(${rotateX}deg) rotateY(${rotateY}deg)`,
-        filter: `brightness(${brightness})`,
-      })
-    })
+    cardRef.current.style =
+      `transform: rotateX(${rotateX}deg) rotateY(${rotateY}deg);` +
+      `filter: brightness(${brightness});`
   }
 
   function handleMouseLeave() {
-    animationFrameRef.current = requestAnimationFrame(() =>
-      setStyles(initialStyles)
-    )
+    cardRef.current.style =
+      'transform: rotateX(0deg) rotateY(0deg); filter: brightness(1)'
   }
 
   return (
@@ -50,7 +38,7 @@ function Card3d({ children }) {
     >
       {React.cloneElement(children, {
         className: `card3d-children ${children.props.className || ''}`,
-        style: { ...styles },
+        ref: cardRef
       })}
     </div>
   )

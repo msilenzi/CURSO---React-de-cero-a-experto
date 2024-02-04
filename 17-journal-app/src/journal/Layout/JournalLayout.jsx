15c4713f522dba557ef7c6d9f5ede59af8a8648e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-04 12:49:42 -0300

    add <ChildrenWrapper /> to <JournalLayout />

--- 17-journal-app/src/journal/Layout/JournalLayout.jsx
+++ 17-journal-app/src/journal/Layout/JournalLayout.jsx
@@ -9,27 +9,40 @@ function JournalLayout({ children }) {
     <Box sx={{ display: 'flex' }}>
       <Navbar drawerWidth={drawerWidth} />
       <Sidebar drawerWidth={drawerWidth} />
-      <Box
-        component="main"
-        sx={{
-          flexGrow: 1,
-          p: 3,
-          pt: 0,
-          display: 'flex',
-          flexDirection: 'column',
-          minHeight: '100vh',
-          width: { xs: '100%', md: `calc(100% - ${drawerWidth}px)` },
-        }}
-      >
-        {/* Fixes problem with content under navbar */}
-        <Toolbar sx={{ mb: 3 }} />
-        {children}
-      </Box>
+      <ChildrenWrapper>{children}</ChildrenWrapper>
+    </Box>
+  )
+}
+
+function ChildrenWrapper({ children }) {
+  return (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        p: 3,
+        pt: 0,
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        width: { xs: '100%', md: `calc(100% - ${drawerWidth}px)` },
+      }}
+    >
+      {/* Fixes problem with content under navbar */}
+      <Toolbar sx={{ mb: 3 }} />
+      {children}
     </Box>
   )
 }
 
 JournalLayout.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+}
+
+ChildrenWrapper.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.node),
     PropTypes.node,

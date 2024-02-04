47989dfa16ab04b715116e6fef0d56acc2e36694
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-04 13:07:52 -0300

    add <NewEntryBtn /> to <JournalPage />

--- 17-journal-app/src/journal/pages/JournalPage.jsx
+++ 17-journal-app/src/journal/pages/JournalPage.jsx
@@ -1,12 +1,35 @@
+import { AddOutlined } from '@mui/icons-material'
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '@Journal/Layout'
 import { NoteView, NothingSelectedView } from '@Journal/views'
 
 function JournalPage() {
   return (
-    <JournalLayout>
-      {/* <NothingSelectedView /> */}
-      <NoteView />
-    </JournalLayout>
+    <>
+      <JournalLayout>
+        {/* <NothingSelectedView /> */}
+        <NoteView />
+      </JournalLayout>
+      <NewEntryBtn />
+    </>
+  )
+}
+
+function NewEntryBtn() {
+  return (
+    <IconButton
+      size="large"
+      sx={{
+        position: 'fixed',
+        right: 28,
+        bottom: 28,
+        color: 'white',
+        bgcolor: 'error.main',
+        ':hover': {bgcolor: 'error.light'}
+      }}
+    >
+      <AddOutlined sx={{ fontSize: 28 }} />
+    </IconButton>
   )
 }
 

ce720008bcfe4b9f07add403af43611c619ba48a
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-12 12:21:36 -0300

    move App to features/counter

--- 18-redux-toolkit/src/main.jsx
+++ 18-redux-toolkit/src/main.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { store } from 'store'
-import App from 'App.jsx'
+import { Counter } from 'features/counter/components'
 
 import 'index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <Counter />
     </Provider>
   </React.StrictMode>
 )
--- 18-redux-toolkit/src/store/store.js
+++ 18-redux-toolkit/src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { counterSlice } from './slices/counter'
+import { counterSlice } from 'features/counter/slices'
 
 export const store = configureStore({
   reducer: {
-    counter:  counterSlice.reducer
+    counter: counterSlice.reducer,
   },
 })

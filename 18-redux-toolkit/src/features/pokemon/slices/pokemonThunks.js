a39e034ddb6c4fc1086baea1d4e3ad2be1d2e897
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-12 21:24:27 -0300

    show pokemons with pagination

--- 18-redux-toolkit/src/features/pokemon/slices/pokemonThunks.js
+++ 18-redux-toolkit/src/features/pokemon/slices/pokemonThunks.js
@@ -3,13 +3,13 @@ import { setPokemons, startLoadingPokemons } from './pokemonSlice'
 
 export function getPokemons(page) {
   const LIMIT = 10
-  const OFFSET = page * 10
+  const OFFSET = (page - 1) * 10
 
   return async (dispatch, getState) => {
     dispatch(startLoadingPokemons())
     const { data } = await pokemonApi.get(
       `/pokemon?limit=${LIMIT}&offset=${OFFSET}`
     )
-    dispatch(setPokemons({ pokemons: data.results, page: page + 1 }))
+    dispatch(setPokemons({ pokemons: data.results, page: page }))
   }
 }

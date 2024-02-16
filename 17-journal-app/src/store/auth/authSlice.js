785cc09619f85578778ea5f14a771b6f92151869
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-15 23:37:04 -0300

    create auth store

---
+++ 17-journal-app/src/store/auth/authSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  /** @type {'checking' | 'not-authenticated' | 'authenticated'} */
+  status: 'not-authenticated',
+  uid: null,
+  email: null,
+  displayName: null,
+  photoURL: null,
+  errorMessage: null,
+}
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    login: (state, action) => {},
+    logout: (state, action) => {},
+    checkingCredentials: (state) => {},
+  },
+})
+
+export const { login, logout, checkingCredentials } = authSlice.actions

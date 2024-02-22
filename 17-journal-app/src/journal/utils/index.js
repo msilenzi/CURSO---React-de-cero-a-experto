f951e19950042dde60268dff7bb237df7c8f186f
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-02-22 19:19:35 -0300

    add timestampToString

---
+++ 17-journal-app/src/journal/utils/index.js
@@ -0,0 +1,1 @@
+export { default as timestampToString } from './timestampToString'
--- 17-journal-app/src/journal/views/NoteView.jsx
+++ 17-journal-app/src/journal/views/NoteView.jsx
@@ -1,10 +1,11 @@
 import { Save } from '@mui/icons-material'
 import { Button, Grid, Stack, TextField, Typography } from '@mui/material'
 import { ImageGallery } from '@Journal/components'
+import { timestampToString } from '@Journal/utils'
 
 function NoteView() {
   return (
-    <Stack spacing={3} sx={{width: '100%', maxWidth: '720px', m: '0 auto'}}>
+    <Stack spacing={3} sx={{ width: '100%', maxWidth: '720px', m: '0 auto' }}>
       <Grid
         container
         direction="row"
@@ -12,7 +13,7 @@ function NoteView() {
         justifyContent="space-between"
       >
         <Typography variant="h6" fontWeight={300}>
-          {new Date().toLocaleString('en-US', { dateStyle: 'full' })}
+          {timestampToString(Date.now())}
         </Typography>
         <Button variant="text" startIcon={<Save />}>
           Save

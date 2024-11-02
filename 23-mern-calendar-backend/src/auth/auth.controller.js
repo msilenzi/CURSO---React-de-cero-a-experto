7c12212ad9725ef5521540049829033932aebeb3
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-02 19:34:38 -0300

    save users in db

--- 23-mern-calendar-backend/src/auth/auth.controller.js
+++ 23-mern-calendar-backend/src/auth/auth.controller.js
@@ -1,9 +1,31 @@
 const { response } = require('express')
-const { validationResult } = require('express-validator')
+const User = require('../models/user.model')
 
-function signup(req, res = response) {
-  const { name, email, password } = req.body
-  return res.status(201).json({ ok: true, action: 'signup', payload: req.body })
+async function signup(req, res = response) {
+  let user = await User.findOne({ email: req.body.email })
+  if (user) {
+    return res
+      .status(400)
+      .json({ ok: false, msg: 'Ya existe un usuario con ese correo' })
+  }
+
+  try {
+    user = new User(req.body)
+    await user.save()
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Ocurrió un error al crear el usuario',
+    })
+  }
+
+  return res.status(201).json({
+    ok: true,
+    payload: {
+      id: user.id,
+      name: user.name,
+    },
+  })
 }
 
 function login(req, res = response) {

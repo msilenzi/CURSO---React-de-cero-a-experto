1a6c219aceead1a607791fc1b172322013862dee
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-02 12:57:29 -0300

    add public and .env

--- 23-mern-calendar-backend/index.js
+++ 23-mern-calendar-backend/index.js
@@ -1,17 +1,23 @@
 const express = require('express')
+require('dotenv').config()
 
 // Crear el servidor de express
 const app = express()
 
+// Directorio público
+//   `use()` permite definir un middleware. Un middleware es una función que
+//   se ejecuta cada vez que alguien hace una petición.
+app.use(express.static('public'))
+
 // Rutas
-app.get('/', (req, res) => {
-  console.log('GET /')
-  return res.json({
-    ok: true,
-  })
-})
+// app.get('/', (req, res) => {
+//   console.log('GET /')
+//   return res.json({
+//     ok: true,
+//   })
+// })
 
 // Escuchar peticiones
-app.listen(4000, () => {
-  console.log(`Servidor corriendo en puerto ${4000}`)
+app.listen(process.env.PORT, () => {
+  console.log(`Servidor corriendo en puerto ${process.env.PORT}`)
 })

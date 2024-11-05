c4a2fd6413f7b985d349e3de7f01912ebf58072e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-04 22:58:03 -0300

    fix /refresh validation

--- 23-mern-calendar-backend/src/routes/auth.routes.js
+++ 23-mern-calendar-backend/src/routes/auth.routes.js
@@ -1,18 +1,18 @@
 // /api/v1/auth
 
 const router = require('express').Router()
-
 const authController = require('../controllers/auth.controller')
 const {
   signupValidation,
   loginValidation,
 } = require('../validations/auth.validations')
 const validateFields = require('../middlewares/validateFields.middleware')
+const { validateJwt } = require('../middlewares/validateJwt.middleware')
 
 router.post('/signup', signupValidation, validateFields, authController.signup)
 
 router.post('/login', loginValidation, validateFields, authController.login)
 
-router.get('/refresh', authController.refreshToken)
+router.get('/refresh', validateJwt, authController.refreshToken)
 
 module.exports = router

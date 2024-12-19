fa3cc5db3b3247f41f51f9bb33be6e4a8318e96c
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-19 11:56:56 -0300

    add event create

--- 23-mern-calendar-backend/src/controllers/events.controller.js
+++ 23-mern-calendar-backend/src/controllers/events.controller.js
@@ -1,4 +1,5 @@
 const { response } = require('express')
+const Event = require('../models/event.model')
 
 function findAll(req, res = response) {
   return res.json({
@@ -9,13 +10,23 @@ function findAll(req, res = response) {
   })
 }
 
-function create(req, res = response) {
-  return res.json({
-    ok: true,
-    payload: {
-      events: 'create',
-    },
-  })
+async function create(req, res = response) {
+  const event = new Event(req.body)
+  event.user = req.id
+
+  try {
+    const savedEvent = await event.save()
+    res.json({
+      ok: true,
+      payload: savedEvent,
+    })
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({
+      ok: false,
+      msg: 'No se pudo crear el evento',
+    })
+  }
 }
 
 function update(req, res = response) {
